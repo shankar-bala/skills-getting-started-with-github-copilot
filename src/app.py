d60275ebd98d99885c8f6659e3c5e6900ebeee2a
Shankar <35820395+shankar-bala@users.noreply.github.com>
2025-04-24 22:27:24 +0000

    Refactor signup logic to validate if student is already signed up for an activity

--- src/app.py
+++ src/app.py
@@ -99,11 +99,10 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-    # Add student
+# Validate student is not already signed up
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Already signed up for this activity")
-    if len(activity["participants"]) >= activity["max_participants"]:
-        raise HTTPException(status_code=400, detail="Activity is full")
+
     # Add the student to the activity
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
